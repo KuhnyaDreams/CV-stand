1cbfd6c6dab2bc57a8f80b0df8d9b32fda456be7
Wobir <ilyakorobi4un@yandex.ru>
2026-05-12 07:10:08 +0500

    Revert "updated "gitignore, fgsm_attack run_gpu_check""
    This reverts commit 538447a1d78e5244cae3693a299e02484316d180.

--- wrapper/run_gpu_check.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-print("TF версия:", tf.__version__)
-gpus = tf.config.list_physical_devices('GPU')
-print(f"Найдено GPU: {len(gpus)}")
-for gpu in gpus:
-    print(f" - {gpu}")
